692bcca78451c321beac152da9711659f5de3c38
cc00ffee <stasikkacper1@gmail.com>
2024-12-29 04:11:45 -0600

    character change

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ function Card() {
     let indexNum = 0;
     let locIndexNum = 0;
     const userArray: string[] = ["cc00ffee!", "0xCC00FFEE!", "3422617582!", "sizeof(cc00ffee)!", 'echo "cc00ffee!"'];
-    const locationArray: string[] = ["Chicago", "41°52′55″N 87°37′40″W", "The Windy City"]
+    const locationArray: string[] = ["Chicago!", "41°52′55″N 87°37′40″W!", "The Windy City!"]
     const [index, setIndex] = useState<number>(0);
     const [locIndex, setLocIndex] = useState<number>(0);
     function handleUserFormat() {
@@ -22,7 +22,7 @@ function Card() {
         <div>
             <h1 className="text-3xl font-bold">Hi! I'm <button onClick={ handleUserFormat }><code className="bg-gray-100 dark:bg-gray-600 p-1 rounded">{ userArray[index] }</code></button></h1>
             <br/>
-            <p className="text-xl font-semibold">I study Computer Science in 🏙️<button onClick={ handleLocFormat }><code className="bg-gray-100 dark:bg-gray-600 p-1 rounded">{ locationArray[locIndex] }</code></button>!<br/>I love playing CTFs! You can view my writeups here.</p>
+            <p className="text-xl font-semibold">I study Computer Science in 🏙️<button onClick={ handleLocFormat }><code className="bg-gray-100 dark:bg-gray-600 p-1 rounded">{ locationArray[locIndex] }</code></button><br/>I love playing CTFs! You can view my writeups here.</p>
             <div className="flex items-start space-x-8 mt-4 font-medium">
                 <span>Discord: cc00ffee</span>
                 <a href="https://twitter.com/_cc00ffee">Twitter</a>
